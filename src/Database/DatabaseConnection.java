cdee756bb3e6e81658cddabb8c7b63160aeaaac4
RestaurantManagementSystem <khuld.eggstra@gmail.com>
2026-05-03 22:12:55 +0500

    Made database and connected it Made few other classes too

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/restaurant_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "mySQL@313";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
